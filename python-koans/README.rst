6ebbdc8446426f7d8687427506af1a886f393260
Wilson Sumanang <wilson.sumanang@zefr.com>
2014-04-07 13:14:07 -0700

    solved python koans

---
+++ python-koans/README.rst
@@ -0,0 +1,37 @@
+*Python Koans Solutions
+Here are the solutions that I created while learning Python TDD
+
+
+Information
+============
+Python Koans
+============
+
+.. image:: http://i442.photobucket.com/albums/qq150/gregmalcolm/PythonKoansScreenshot.png
+
+Python Koans is an interactive tutorial for learning the Python programming
+language by making tests pass.
+
+Most tests are 'fixed' by filling the missing parts of assert functions. Eg:
+
+    self.assertEqual(__, 1+2)
+
+which can be fixed by replacing the __ part with the appropriate code:
+
+    self.assertEqual(3, 1+2)
+
+Occasionally you will encounter some failing tests that are already filled out.
+In these cases you will need to finish implementing some code to progress. For
+example, there is an exercise for writing some code that will tell you if a
+triangle is equilateral, isosceles or scalene.
+
+As well as being a great way to learn some Python, it is also a good way to get
+a taste of Test Driven Development (TDD).
+
+
+Downloading Python Koans
+------------------------
+
+Python Koans is available through git on Github:
+
+    http://github.com/gregmalcolm/python_koans
---
+++ python-koans/python2/koans/about_sets.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from runner.koan import *
+
+
+class AboutSets(Koan):
+    def test_sets_make_keep_lists_unique(self):
+        highlanders = ['MacLeod', 'Ramirez', 'MacLeod', 'Matunas',
+            'MacLeod', 'Malcolm', 'MacLeod']
+
+        there_can_only_be_only_one = set(highlanders)
+
+        self.assertEqual(set(highlanders), there_can_only_be_only_one)
+
+    def test_sets_are_unordered(self):
+        self.assertEqual(set(['1', '3', '2', '5', '4']), set('12345'))
+
+    def test_convert_the_set_into_a_list_to_sort_it(self):
+        self.assertEqual(['1', '2', '3', '4', '5'], sorted(set('13245')))
+
+    # ------------------------------------------------------------------
+
+    def test_set_have_arithmetic_operators(self):
+        scotsmen = set(['MacLeod', 'Wallace', 'Willie'])
+        warriors = set(['MacLeod', 'Wallace', 'Leonidas'])
+
+        self.assertEqual(set(['Willie']), scotsmen - warriors)
+        self.assertEqual(set(['Wallace', 'MacLeod', 'Willie', 'Leonidas']), scotsmen | warriors)
+        self.assertEqual(set(['MacLeod', 'Wallace']), scotsmen & warriors)
+        self.assertEqual(set(['Willie', 'Leonidas']), scotsmen ^ warriors)
+
+    # ------------------------------------------------------------------
+
+    def test_we_can_query_set_membership(self):
+        self.assertEqual(True, 127 in set([127, 0, 0, 1]))
+        self.assertEqual(True, 'cow' not in set('apocalypse now'))
+
+    def test_we_can_compare_subsets(self):
+        self.assertEqual(True, set('cake') <= set('cherry cake'))
+        self.assertEqual(True, set('cake').issubset(set('cherry cake')))
+
+        self.assertEqual(False, set('cake') > set('pie'))
