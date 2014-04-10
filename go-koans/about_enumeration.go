2d3a8713e82843c7aedac070e47b09cdc3f1efe9
Wilson Sumanang <wilson.sumanang@zefr.com>
2014-04-10 11:50:04 -0700

    solve go koans

--- go-koans/about_enumeration.go
+++ go-koans/about_enumeration.go
@@ -11,8 +11,8 @@ func aboutEnumeration() {
       concatenated += v
     }
 
-    assert(concatenated == __string__) // for loops have a modern variation
-    assert(total == __int__)           // which offers both a value and an index
+    assert(concatenated == "hello world!") // for loops have a modern variation
+    assert(total == 3)           // which offers both a value and an index
   }
 
   {
@@ -22,7 +22,6 @@ func aboutEnumeration() {
     for _, v := range strings {
       totalLength += len(v)
     }
-
-    assert(totalLength == __int__) // although we may omit either value
+    assert(totalLength == 12) // although we may omit either value
   }
 }
